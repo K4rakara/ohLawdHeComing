92c317ac05609e698bd237d52db2c002697a7879
Karakara「Cubone」 <40474474+K4rakara@users.noreply.github.com>
2019-09-16 17:16:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 var ohLawd = {
-  win : document.defaultView,
-  ox : 0,
-  oy : 0,
-  x : 0,
-  y : 0,
-  width : 100,
-  height : 100
+    win: document.defaultView,
+    ox: 0,
+    oy: 0,
+    x: 0,
+    y: 0,
+    width: 100,
+    height: 100
 };
+
 function ohLawd_updateCoords() {
-  ohLawd.x = ohLawd.ox - (ohLawd.width/2);
-  ohLawd.y = ohLawd.oy - (ohLawd.height/2);
+    ohLawd.x = ohLawd.ox - (ohLawd.width / 2);
+    ohLawd.y = ohLawd.oy - (ohLawd.height / 2);
 }
+
 function ohLawd_update() {
-  ohLawd.width = ohLawd.width + 1;
-  ohLawd.height = ohLawd.height + 1;
-  ohLawd_updateCoords();
-  ohLawd.win.resizeTo(ohLawd.width,ohLawd.height);
-  ohLawd.win.moveTo(ohLawd.x,ohLawd.y);
-  if (ohLawd.width >= screen.availWidth || ohLawd.height >= screen.availHeight) {
-    ohLawd.ox = Math.random() * screen.width;
-    ohLawd.oy = Math.random() * screen.height;
-    ohLawd.width = 100;
-    ohLawd.height = 100;
-  }
+    ohLawd.width = ohLawd.width + 1;
+    ohLawd.height = ohLawd.height + 1;
+    ohLawd_updateCoords();
+    ohLawd.win.resizeTo(ohLawd.width, ohLawd.height);
+    ohLawd.win.moveTo(ohLawd.x, ohLawd.y);
+    if (ohLawd.width >= screen.availWidth || ohLawd.height >= screen.availHeight) {
+        ohLawd.ox = Math.random() * screen.width;
+        ohLawd.oy = Math.random() * screen.height;
+        ohLawd.width = 100;
+        ohLawd.height = 100;
+    }
 }
+
 ohLawd.ox = Math.random() * screen.width;
 ohLawd.oy = Math.random() * screen.height;
-ohLawd.win.resizeTo(ohLawd.width,ohLawd.height);
-ohLawd.win.moveTo(ohLawd.x,ohLawd.y);
-setInterval(ohLawd_update,100);
+ohLawd.win.resizeTo(ohLawd.width, ohLawd.height);
+ohLawd.win.moveTo(ohLawd.x, ohLawd.y);
+setInterval(ohLawd_update, 10);
