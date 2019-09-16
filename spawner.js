d585c6e15e5c4c28ff8c89af3a961d2973c89138
Karakara「Cubone」 <40474474+K4rakara@users.noreply.github.com>
2019-09-16 17:13:33 -0400

    Update spawner.js

--- spawner.js
+++ spawner.js
@@ -1,1 +1,1 @@
-var createdWindow = window.open('k4rakara.github.io/ohLawdHeComing','','width = 100, height = 100');
+var createdWindow = window.open('k4rakara.github.io/ohLawdHeComing/index.html','','width = 100, height = 100');
